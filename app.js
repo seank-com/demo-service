f736dc0228fc3cdeb1ae74a78e6fc82d4b499399
Sean Kelly <home@seank.com>
2016-03-25 13:49:26 -0700

    A simple node server from http://nodejs.org/

---
+++ app.js
@@ -0,0 +1,10 @@
+const http = require('http');
+
+const port = 4000;
+
+http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Hello World From Node JS\n");
+}).listen(port, () => {
+  console.log(`Server listening on port ${port}.`);
+});
